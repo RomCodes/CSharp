d148144e37d630e0b88e811a24f573b6d704f367
RomishShrestha <romishshrestha@gmail.com>
2024-02-02 22:32:00 +1100

    first csharp program

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Dynamic;
+using System.Numerics;
+
+namespace OperatorExpressionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x = 1;
+            y = 2;
+            z = 3000232;
+
+            Console.WriteLine("Choose a variable x, y, z: ");
+            string userChoice = Console.ReadLine();
+
+
+            Console.WriteLine("Choose a multiplier: ");
+            string  userMultiplier = Console.ReadLine();
+
+            if(!BigInteger.TryParse(userMultiplier, out BigInteger  userMultiplierInt))
+            {
+                Console.WriteLine("Invalid multiplier chosen. Please choose an integer value for the multiplier!!!");
+                return;
+            }
+
+
+
+            BigInteger result = 0;
+            bool validChoice = true;
+
+
+            switch(userChoice)
+            {
+                case "x": result = x * userMultiplierInt;
+                    break;
+
+                case "y":
+                    result = y * userMultiplierInt; 
+                    break;
+
+                case "z":
+                    result = z * userMultiplierInt;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Choice!!");
+                    validChoice = false;
+                    break;
+
+            }
+
+            if (validChoice)
+            {
+
+                Console.WriteLine($"Your chosen variable {userChoice} and multiplier {userMultiplierInt}");
+            }   Console.WriteLine($"The answer is {result}");
+    }
+    }
+}
