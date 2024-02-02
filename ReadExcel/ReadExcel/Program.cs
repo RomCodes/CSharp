3c0953f6b1f8157bf2699462c8c60b09fcf41d32
RomishShrestha <romishshrestha@gmail.com>
2024-02-03 06:56:52 +1100

    C# program to read excel file

---
+++ ReadExcel/ReadExcel/Program.cs
@@ -0,0 +1,46 @@
+﻿using ClosedXML;
+using ClosedXML.Excel;
+
+
+
+class ReadingExcelFile
+{
+    public static void readExcelFile(string pathToFile)
+    {
+
+        using (XLWorkbook wb = new XLWorkbook(pathToFile))
+        {
+
+            IXLWorksheet ws = wb.Worksheet(1);
+            foreach (IXLRow row in ws.RowsUsed())
+            {
+                foreach (IXLCell cell in row.CellsUsed())
+                {
+                    Console.WriteLine(cell.Value.ToString());
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+class RunReadingExcelFile
+{
+    static void Main(string[] args)
+
+    {
+
+        string xlPath = @"C:\Users\User\OneDrive\Documents\Book1.xlsx";
+
+        //call the program
+        ReadingExcelFile.readExcelFile(xlPath);
+
+    }
+}
